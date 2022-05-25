2809d893313e1c5d896d1d7d6bdd4cae6906c5bc
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 07:40:47 -0700

    Update yt-comment.js

--- plugins/yt-comment.js
+++ plugins/yt-comment.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'No Text'
+  if (!text) throw '*کامنت وارد نشده!*'
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
     avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
     comment: text,
     username: conn.getName(m.sender)
-  }), 'yt-comment.png', 'Here is your comment', m)
+  }), 'yt-comment.png', 'کامنتت اینجاست :)', m)
 }
 
 handler.help = ['ytcomment <comment>']
