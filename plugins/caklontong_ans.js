bde7494e0c67237977de91517d7a3e216a020294
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 09:54:55 -0700

    Update caklontong_ans.js

--- plugins/caklontong_ans.js
+++ plugins/caklontong_ans.js
@@ -3,9 +3,9 @@ const threshold = 0.72
 module.exports = {
     async before(m) {
         let id = m.chat
-        if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*calo/i.test(m.quoted.contentText)) return !0
+        if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*calo/i.test(m.quoted.contentText)) return !0
         this.caklontong = this.caklontong ? this.caklontong : {}
-        if (!(id in this.caklontong)) return m.reply('Soal itu telah berakhir')
+        if (!(id in this.caklontong)) return m.reply('That matter is over')
         if (m.quoted.id == this.caklontong[id][0].id) {
             let json = JSON.parse(JSON.stringify(this.caklontong[id][1]))
             if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
@@ -15,7 +15,7 @@ module.exports = {
                 clearTimeout(this.caklontong[id][3])
                 delete this.caklontong[id]
             } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-            else m.reply(`*Salah!*`)
+            else m.reply(`*Wrong!*`)
         }
         return !0
     },
