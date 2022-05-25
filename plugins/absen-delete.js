7e1c28011d9d39d8a47d681e66c280829886ffd8
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 06:13:10 -0700

    Update absen-delete.js

--- plugins/absen-delete.js
+++ plugins/absen-delete.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, usedPrefix, isAdmin, isOwner }) => {
     }
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
-    if (!(id in conn.absen)) return conn.sendButton(m.chat, `Tidak ada absen berlangsung!`, '© wabot-aq', 'Mulai', `${usedPrefix}+absen`, m)
+    if (!(id in conn.absen)) return conn.sendButton(m.chat, `No absences take place!`, '© surena', 'Begin', `${usedPrefix}+absen`, m)
     delete conn.absen[id]
-    m.reply(`Absen berhasil dihapus`)
+    m.reply(`Absenteeism successfully removed`)
 }
 handler.help = ['hapusabsen']
 handler.tags = ['absen']
