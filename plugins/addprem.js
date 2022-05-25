d9952549a0a2ff3c1a1dc25785574f1408e81c4a
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 06:33:46 -0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `tag orangnya!`
-    if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
+    if (!who) throw `Tag a person!`
+    if (global.prems.includes(who.split`@`[0])) throw 'He is already an premium!'
     global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} sekarang premium!`, m, {
+    conn.reply(m.chat, `@${who.split`@`[0]} is now premium!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
