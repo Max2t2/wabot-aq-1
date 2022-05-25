da2cc314ff6bece85186a16ca6ca7b81c5b5344d
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 06:30:49 -0700

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -3,19 +3,19 @@ let { WAMessageProto } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/\+|add/i, '')
-    if (!m.quoted) throw 'Balas pesan!'
-    if (!text) return conn.sendButton(m.chat, `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} tes`, '© wabot-aq', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
+    if (!m.quoted) throw 'Reply to a message!'
+    if (!text) return conn.sendButton(m.chat, `Witch text?\n\nFor example:\n${usedPrefix + command} test`, '© surena', 'Message List', `${usedPrefix}list${which}`, m)
     let msgs = db.data.msgs
-    if (text in msgs) return conn.sendButton(m.chat, `'${text}' telah terdaftar, gunakan nama lain!`, '© wabot-aq', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
+    if (text in msgs) return conn.sendButton(m.chat, `'${text}' have registered, use another name!`, '© surena', 'Message List', `${usedPrefix}list${which}`, m)
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    if (db.data.chats[m.chat].getmsg) return m.reply(`Berhasil menambahkan pesan dengan nama '${text}'
+    if (db.data.chats[m.chat].getmsg) return m.reply(`Successfully added a message with a name '${text}'
     
-Akses dengan mengetik '${text}'`)
-    else return conn.sendButton(m.chat, `Berhasil menambahkan pesan dengan nama '${text}'
+Access by typing '${text}'`)
+    else return conn.sendButton(m.chat, `Successfully added a message with a name '${text}'
 
-Akses dengan ${usedPrefix}get${which} ${text}
+Access with ${usedPrefix}get${which} ${text}
 
-Jika Getmsg diaktifkan maka tidak perlu lagi mengetik *${usedPrefix}get${which}*`, '© wabot-aq', 'Aktifkan', '.1 getmsg', m)
+If Getmsg is enabled then there is no need to type anymore *${usedPrefix}get${which}*`, '© surena', 'Enable', '.1 getmsg', m)
 }
 handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <teks>')
 handler.tags = ['database']
