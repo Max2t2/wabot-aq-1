6f715a8c191d8e13d7ff38ec48f6026fa85997ff
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 05:47:52 -0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -4,10 +4,11 @@ module.exports = {
         this.spam = this.spam ? this.spam : {}
         if (m.sender in this.spam) {
             this.spam[m.sender].count++
-            if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 7) {
-                if (this.spam[m.sender].count > 7) {
-                    global.db.data.users[m.sender].banned = true
+            if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 9) {
+                if (this.spam[m.sender].count > 9) {
+                    //global.db.data.users[m.sender].banned = true
                     m.reply('*در این گروه مجاز به اسپم نیستید!!*')
+                    this.groupRemove(m.chat, [m.sender])
                 }
                 this.spam[m.sender].count = 0
                 this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
