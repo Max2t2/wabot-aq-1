ae41d1a4d781ee4838debc36515638266e7ad316
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 10:13:18 -0700

    Update warning_del.js

--- plugins/warning_del.js
+++ plugins/warning_del.js
@@ -1,13 +1,13 @@
 let handler = async (m) => {
   let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-  if (!mention) throw `Tag/mention orangnya!`
+  if (!mention) throw `Tag/mention the person!`
   let warn = global.db.data.users[mention].warn
   if (warn > 0) {
     global.db.data.users[mention].warn -= 1
     m.reply('⚠️ *WARNING -1*')
-    m.reply(`Admin mengurangi warn kamu, warn kamu sekarang ${warn - 1}`, mention)
+    m.reply(`Admin reduced your warn, now your warn is: ${warn - 1}`, mention)
   } else if (warn == 0) {
-    m.reply('User tidak memiliki warn')
+    m.reply('The user does not have any warn')
   }
 }
 
