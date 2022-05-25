4ef476df2bf8587a9fe8db619b09cfb79ea72324
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 06:18:40 -0700

    Update absen.js

--- plugins/absen.js
+++ plugins/absen.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
-    if (!(id in conn.absen)) return conn.sendButton(m.chat, `Tidak ada absen berlangsung!`, '© wabot-aq', 'Mulai', `${usedPrefix}+absen`, m)
+    if (!(id in conn.absen)) return conn.sendButton(m.chat, `No absences take place!`, '© surena', 'Begin', `${usedPrefix}+absen`, m)
     let absen = conn.absen[id][1]
     const wasVote = absen.includes(m.sender)
-    if (wasVote) throw 'Kamu sudah absen!'
+    if (wasVote) throw 'Youre gone!'
     absen.push(m.sender)
     let d = new Date
     let date = d.toLocaleDateString('id', {
@@ -14,14 +14,14 @@ let handler = async (m, { conn, usedPrefix }) => {
     })
     let list = absen.map((v, i) => `│ ${i + 1}.  @${v.split`@`[0]}`).join('\n')
     let caption = `
-Tanggal: ${date}
+date: ${date}
 
 ${conn.absen[id][2] ? conn.absen[id][2] + '\n' : ''}
-╭─「 Daftar Absen 」
+╭─「 Absen List 」
 │ Total: ${absen.length}
 ${list}
 ╰────`.trim()
-    await conn.send2Button(m.chat, caption, '© wabot-aq', 'Absen', `${usedPrefix}absen`, 'Cek', `${usedPrefix}cekabsen`, m)
+    await conn.send2Button(m.chat, caption, '© surena', 'Absen', `${usedPrefix}absen`, 'Check', `${usedPrefix}cekabsen`, m)
 }
 handler.help = ['absen']
 handler.tags = ['absen']
