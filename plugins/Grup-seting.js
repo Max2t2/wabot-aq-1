600836e845e4de142b838a5ea80a5d17de2c52aa
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-25 23:25:33 -0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*Please select the option :*
+*Please select an option :*
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
