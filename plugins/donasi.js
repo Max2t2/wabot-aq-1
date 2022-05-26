cb9bfe37c86cdbac7d0a0271e5670e0c8bce4941
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-26 08:56:46 -0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085895086040]
-│ • Telkomsel [081334177340]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/Nurutomo
-│ • https://saweria.co/ariffb
+╭─「 Support • Developer 」
+│ • Developer : wa.me/989301860610
+│ • Support : wa.me/989389383634
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
