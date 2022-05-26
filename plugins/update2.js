6c862678bde1035caaf73104d90a38dc6b6a20a5
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-26 06:17:05 -0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'Nurutomo/wabot-aq'
+let repository = 'hyno831/wabot-aq'
 let branch = 'master'
 
 async function handler(m, { text }) {
