1338e0497430d93d0d0d0dc78992b6e4c594631d
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-27 05:44:04 -0700

    Update inspectlink.js

--- plugins/inspectlink.js
+++ plugins/inspectlink.js
@@ -20,15 +20,17 @@ let handler = async (m, { conn, text }) => {
 ${res.desc ? `*description:*
 ${res.desc}` : '*There is no description!*'}
 
-`//.trim()
- // let pp = await conn.getProfilePicture(res.id).catch(console.error)
-//if (pp) conn.sendFile(m.chat, pp, 'pp.jpg', null, m)
- // m.reply(caption, false, {
- //   contextInfo: {
- //     mentionedJid: conn.parseMention(caption)
-//    }
-//  })
-//}
+*JSON Version*
+\`\`\`${JSON.stringify(res, null, 1)}\`\`\`
+`.trim()
+  let pp = await conn.getProfilePicture(res.id).catch(console.error)
+  if (pp) conn.sendFile(m.chat, pp, 'pp.jpg', null, m)
+  m.reply(caption, false, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(caption)
+    }
+  })
+}
 handler.help = ['inspect <chat.whatsapp.com>']
 handler.tags = ['tools']
 
