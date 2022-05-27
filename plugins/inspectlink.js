f4b273c20466a82674e822708f59474903fbd3af
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-27 05:41:05 -0700

    Update inspectlink.js

--- plugins/inspectlink.js
+++ plugins/inspectlink.js
@@ -10,27 +10,25 @@ let handler = async (m, { conn, text }) => {
   if (!res) throw res
   let caption = `
 -- [Group Link Inspector] --
-${res.id}
+
 *Group name:* ${res.subject}
-*Group ID:* @${res.id.split('-')[0]} created on *${formatDate(res.creation * 1000)}*${res.subjectOwner ? `
-*Created by* @${res.subjectOwner.split`@`[0]} on *${formatDate(res.subjectTime * 1000)}*`: ''}${res.descOwner ? `
-*Description changed* by @${res.descOwner.split`@`[0]} on *${formatDate(res.descTime * 1000)}*` : ''}
+*Group ID:* @${res.id.split('-')[0]} *Made on:* *${formatDate(res.creation * 1000)}*${res.subjectOwner ? `
+*Created by:* @${res.subjectOwner.split`@`[0]} on *${formatDate(res.subjectTime * 1000)}*`: ''}${res.descOwner ? `
+*Description changed:* by @${res.descOwner.split`@`[0]} on *${formatDate(res.descTime * 1000)}*` : ''}
 *Number of Members:* ${res.size}
 \n*Known members who joined*: ${res.participants ? '\n' + res.participants.map((user, i) => ++i + '. @' + user.id.split`@`[0]).join('\n').trim() : 'None'}
 ${res.desc ? `*description:*
 ${res.desc}` : '*There is no description!*'}
 
-*JSON Version*
-\`\`\`${JSON.stringify(res, null, 1)}\`\`\`
-`.trim()
-  let pp = await conn.getProfilePicture(res.id).catch(console.error)
-  if (pp) conn.sendFile(m.chat, pp, 'pp.jpg', null, m)
-  m.reply(caption, false, {
-    contextInfo: {
-      mentionedJid: conn.parseMention(caption)
-    }
-  })
-}
+`//.trim()
+ // let pp = await conn.getProfilePicture(res.id).catch(console.error)
+//if (pp) conn.sendFile(m.chat, pp, 'pp.jpg', null, m)
+ // m.reply(caption, false, {
+ //   contextInfo: {
+ //     mentionedJid: conn.parseMention(caption)
+//    }
+//  })
+//}
 handler.help = ['inspect <chat.whatsapp.com>']
 handler.tags = ['tools']
 
