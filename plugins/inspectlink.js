346cf612504576bc0076aee696ebf3a06e2e13fb
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-27 05:23:18 -0700

    Update inspectlink.js

--- plugins/inspectlink.js
+++ plugins/inspectlink.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text }) => {
 ${res.id}
 *Group name:* ${res.subject}
 *Group ID:* @${res.id.split('-')[0]} pada *${formatDate(res.creation * 1000)}*${res.subjectOwner ? `
-*Created by* @${res.subjectOwner.split`@`[0]} on ${res.subjectOwner ? `
+*Created by* @${res.subjectOwner.split`@`[0]} on *${formatDate(res.creation * 1000)}*${res.subjectOwner ? `
 *Deskripsi diubah* oleh @${res.descOwner.split`@`[0]} pada *${formatDate(res.descTime * 1000)}*` : ''}
 *Jumlah Member:* ${res.size}
 *Member yang diketahui join*: ${res.participants ? '\n' + res.participants.map((user, i) => ++i + '. @' + user.id.split`@`[0]).join('\n').trim() : 'Tidak ada'}
