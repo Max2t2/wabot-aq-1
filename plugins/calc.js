9fcefa587007f51d19c071d7c777574e5bab3c23
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-26 23:35:38 -0700

    Update calc.js

--- plugins/calc.js
+++ plugins/calc.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   if (id in conn.math) {
     clearTimeout(conn.math[id][3])
     delete conn.math[id]
-    m.reply('متوجه نشدم؟'
+    m.reply('Hmmm...ngecheat?')
   }
   let val = text
     .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
@@ -26,8 +26,8 @@ let handler = async (m, { conn, text }) => {
     if (!result) throw result
     m.reply(`*${format}* = _${result}_`)
   } catch (e) {
-    if (e == undefined) throw '*چه عملیاتی انجام بشه؟*\nFor example: .calc 1+1'
-    throw 'Incorrect format, only 0-9 and -, +, *, /, ×, ÷, π, e, (, ) symbols supported'
+    if (e == undefined) throw 'Isinya?'
+    throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ) yang disupport'
   }
 }
 handler.help = ['calc <expression>']
