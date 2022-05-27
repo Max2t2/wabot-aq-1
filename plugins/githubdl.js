040edb35c8cadd74ef27b58dcaef8d4217e0b60c
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-26 23:29:11 -0700

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -5,7 +5,7 @@ if (!args[1]) throw '*Which repo you want?*\n\nLike:\n${usedPrefix}githubdl <use
 
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
 //ByRizkyAdi
-m.reply(`compressing data to file zip*`)
+m.reply(`compressing data to *zip* file`)
 conn.sendFile( m.chat, url, 'repo.zip', null, m)
 
 }
