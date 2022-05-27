e814f9be1537fdec2557e467c1392cf5572cd6cd
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-27 00:17:44 -0700

    Update togif.js

--- plugins/togif.js
+++ plugins/togif.js
@@ -1,14 +1,14 @@
 let { webp2mp4 } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
+    if (!m.quoted) throw `reply to stickers with caption *${usedPrefix + command}*`
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
+    if (!/webp/.test(mime)) throw `reply to stickers with caption *${usedPrefix + command}*`
     let media = await m.quoted.download()
     let out = Buffer.alloc(0)
     if (/webp/.test(mime)) {
         out = await webp2mp4(media)
     }
-    await conn.sendFile(m.chat, out, 'out.gif', '*© stikerin*', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
+    await conn.sendFile(m.chat, out, 'out.gif', '*© surena*', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['togif (reply)']
 handler.tags = ['sticker']
