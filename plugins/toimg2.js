dc01c2f78dc3563789b65928ffef8c11d49a8e6b
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-27 03:13:07 -0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -1,15 +1,15 @@
 
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-  if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  if (!m.quoted) throw `Reply to sticker! *${usedPrefix + command}*`
   let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  if (!/webp/.test(mime)) throw `Reply to sticker! *${usedPrefix + command}*`
   let media = await m.quoted.download()
   let out = Buffer.alloc(0)
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
+  await conn.sendFile(m.chat, out, 'out.png', '*SURENA AI*', m, false, {
     thumbnail: Buffer.alloc(0)
   })
 }
