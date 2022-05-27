d8dcb0742079d7cdb5abd3ab963e78413ddef77e
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-26 23:28:27 -0700

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,7 +1,7 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
 if (!args[0]) throw 'Enter user name'
-if (!args[1]) throw 'Which repo you want\n\nFor example:\n?'
+if (!args[1]) throw '*Which repo you want?*\n\nLike:\n${usedPrefix}githubdl <username> <repo>'
 
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
 //ByRizkyAdi
