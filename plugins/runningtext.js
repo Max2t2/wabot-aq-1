368f0c727639bdcafba6bc57318e42e36f27c07f
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-28 08:25:02 -0700

    Update runningtext.js

--- plugins/runningtext.js
+++ plugins/runningtext.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!/image/.test(mime)) throw `Balas gambarnya`
+  if (!/image/.test(mime)) throw `*برروی یک تصویر ریپلای بزنید*`
   try { q = m.quoted.download() }
   catch (e) { q = m.download() }
-  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
-  running(await q).then(vid => conn.sendFile(m.chat, vid, 'run.mp4', '*© Nurutomo*\nMade with FFmpeg', m))
+  m.reply('_در حال ساخت..._\n*لطفا چند لحظه صبر کنید*')
+  running(await q).then(vid => conn.sendFile(m.chat, vid, 'run.mp4', '*© surena*\nMade with SURENA AI', m))
 }
 handler.help = ['run']
 handler.tags = ['tools']
