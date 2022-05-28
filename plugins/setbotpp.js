244fe49da36ece9371c987f4ab6c9833ab94659a
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-28 09:15:08 -0700

    Update setbotpp.js

--- plugins/setbotpp.js
+++ plugins/setbotpp.js
@@ -7,12 +7,12 @@
 let handler = async (m, { conn, args }) => {
     let bot = conn.user.jid // Bot
     let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
+    let mime = (q.msg || q).mimetype || 'نام جدید ربات را وارد کنید'
     if (/image/.test(mime)) {
         let img = await q.download()
         if (!img) throw `Foto tidak ditemukan`
         conn.updateProfilePicture(bot, img)
-        conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
+        conn.reply(m.chat, '*✅ نام ربات با موفقیت تغییر کرد*', m)
     }
 }
 handler.help = ['setbotpp']
