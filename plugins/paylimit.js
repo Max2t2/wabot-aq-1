41c7ba10f0482e3dc5a5638fcd00063d496c1c52
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-28 04:34:37 -0700

    Update paylimit.js

--- plugins/paylimit.js
+++ plugins/paylimit.js
@@ -4,17 +4,17 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw '*شخص مورد نظر را تگ کنید*'
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw 'Masukkan jumlah Limit yang akan diberi'
-    if (isNaN(txt)) throw 'Hanya angka'
+    if (!txt) throw 'مقدار Limit برای انتقال را وارد کنید'
+    if (isNaN(txt)) throw 'مخصوص شماره ها'
     let poin = parseInt(txt)
     let limit = poin
     let pjk = Math.ceil(poin * pajak)
     limit += pjk
     if (limit < 1) throw 'Minimal 1'
     let users = global.db.data.users
-    if (limit > users[m.sender].limit) throw 'Limit tidak mencukupi untuk mentransfer'
+    if (limit > users[m.sender].limit) throw 'شما Limit مورد نیاز برای انتقال را ندارید!'
     users[m.sender].limit -= limit
     users[who].limit += poin
 
