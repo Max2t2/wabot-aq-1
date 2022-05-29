0a99c5b0d600784964fe84839998b4464f71f505
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 06:14:10 -0700

    Update Disabled_army.js

--- plugins/Disabled_army.js
+++ plugins/Disabled_army.js
@@ -4,7 +4,8 @@ let handler = async (m, { conn, text }) => {
   await conn.sendFile(m.chat, global.API('xteam', '/videomaker/army', { text: teks }, 'APIKEY'), 'army.mp4', teks, m)
 }
 handler.help = ['army'].map((v) => v + " <text>")
-handler.tags = ['videomaker']
+handler.tags = ['disabled']
+handler.owner = true
 
 handler.command = /^army$/i
 
