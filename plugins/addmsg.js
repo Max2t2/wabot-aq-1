54c53e062ca278d6b2e48fa7fbb6fc739b6784e3
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 03:06:34 -0700

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -10,10 +10,10 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
     if (db.data.chats[m.chat].getmsg) return m.reply(`پیام شما با موفقیت با نام *${text}* ذخیره شد ✅
     
-برای دسترسی از *${text}* استفاده کنید`)
+برای دسترسی از *${text}* استفاده کنید.`)
     else return conn.sendButton(m.chat, `پیام شما با موفقیت با نام *${text}* ذخیره شد ✅
 
-از کد get${which}${usedPrefix} ${text} برای مشاهده استفاده کنید
+از کد ${usedPrefix}get${which}${usedPrefix} ${text} برای مشاهده استفاده کنید.
 
 اگر قابلیت Getmsg فعال باشد دیگر نیازی به استفاده از *get${which}${usedPrefix}* ندارید!`, '© surena', 'فعالسازی', '.1 getmsg', m)
 }
