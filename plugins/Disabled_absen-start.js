700bf32041a43a315329f7a2f3ff5c155112462c
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 02:31:48 -0700

    Update and rename absen-start.js to Disabled_absen-start.js

--- plugins/Disabled_absen-start.js
+++ plugins/Disabled_absen-start.js
@@ -14,5 +14,6 @@ let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
 handler.help = ['mulaiabsen [teks]']
 handler.tags = ['absen']
 handler.command = /^(start|mulai|\+)absen$/i
+handler.owner = true
 
 module.exports = handler
