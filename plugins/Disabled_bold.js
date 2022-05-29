e29ec3462e56c7022571db4292f5e6f4ec70972a
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 08:12:51 -0700

    Update and rename bold.js to Disabled_bold.js

--- plugins/Disabled_bold.js
+++ plugins/Disabled_bold.js
@@ -4,8 +4,9 @@ let handler = async (m, { conn, text }) => {
   await conn.sendFile(m.chat, global.API('xteam', '/videomaker/bold', { text: teks }, 'APIKEY'), 'bold.mp4', teks, m)
 }
 handler.help = ['bold'].map((v) => v + " <text>")
-handler.tags = ['videomaker']
+handler.tags = ['disabled']
 
 handler.command = /^bold$/i
+handler.owner
 
 module.exports = handler
