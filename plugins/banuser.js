1937a7088a54ca910f32c18ce069baae3e1335f3
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 08:09:30 -0700

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Who wants to be banned?'
+    if (!text) throw 'چه کسی بن شود؟'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag User'
+    if (!who) throw 'یک کاربر را تگ کنید'
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `successfully banned user from using surena`, m)
+    conn.reply(m.chat, `*کاربر از دسترسی به سورنا بن شد ✅*`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
