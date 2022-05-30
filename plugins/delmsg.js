d01cfd3e58c511f502c2fe8562661a1245d7c4d5
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 23:35:28 -0700

    Update delmsg.js

--- plugins/delmsg.js
+++ plugins/delmsg.js
@@ -1,10 +1,10 @@
 let handler = async (m, { command, usedPrefix, text }) => {
     let which = command.replace(/get/i, '')
-    if (!text) return conn.sendButton(m.chat, `Witch message?\n\nFor example:\n${usedPrefix + command} test`, '© surena', 'Messages List', `${usedPrefix}list${which}`, m)
+    if (!text) return conn.sendButton(m.chat, `چه پیامی حذف شود؟\n\nبرای مثال: ${command + usedPrefix} test`, '© surena', 'Messages List', `${usedPrefix}list${which}`, m)
     let msgs = global.db.data.msgs
-    if (!text in msgs) return conn.sendButton(m.chat, `'${text}' not registered!`, '© surena', 'Messages List', `${usedPrefix}list${which}`, m)
+    if (!text in msgs) return conn.sendButton(m.chat, `پیامی با نام ${text} وجود ندارد!`, '© surena', 'Messages List', `${usedPrefix}list${which}`, m)
     delete msgs[text]
-    m.reply(`Successfully deleted message! message name: '${text}'`)
+    m.reply(`پیام شما با نام '*${text}* با موفقیت حذف شد ✅`)
 }
 handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'del' + v + ' <teks>')
 handler.tags = ['database']
