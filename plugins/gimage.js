a26f137b80eeb9858bbebf155bc34c4a5a98bbd5
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-30 01:54:41 -0700

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -3,10 +3,10 @@ let _gis = require('g-i-s')
 let gis = promisify(_gis)
 
 let handler  = async (m, { conn, args, text }) => {
-  if (!text) throw 'What do you searching for?'
+  if (!text) throw 'به دنبال چه چیزی هستید؟'
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
-  if (!url) throw '404 Not Found'
+  if (!url) throw '~404~ Not Found'
   conn.sendFile(m.chat, url, 'gimage', `
 *── 「 GOOGLE IMAGE 」 ──*
 
