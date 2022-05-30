cdb0128a1bb9852177b671afacc5edb8e8444f93
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-30 01:53:06 -0700

    Update and rename getsider.js to Disabled_getsider.js

--- plugins/Disabled_getsider.js
+++ plugins/Disabled_getsider.js
@@ -20,7 +20,8 @@ ${members > 1 ? `${members - reads.length - deliveries.length} remaining` : ''}
   })
 }
 handler.help = ['getsider']
-handler.tags = ['group']
+handler.tags = ['disabled']
+handler.owner = true
 
 handler.command = /^getsider$/
 
