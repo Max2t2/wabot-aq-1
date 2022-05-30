439e58615687de80f04e478795ef5b786ab71401
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-30 01:42:33 -0700

    Update getexif.js

--- plugins/getexif.js
+++ plugins/getexif.js
@@ -2,7 +2,7 @@ let webp = require('node-webpmux')
 let util = require('util')
 
 let handler = async (m) => {
-    if (!m.quoted) return m.reply('*Reply to sticker*')
+    if (!m.quoted) return m.reply('*برروی استیکر مورد نظر ریپلای کنید*')
     let q = { message: { [m.quoted.mtype]: m.quoted } }
     if (/sticker/.test(m.quoted.mtype)) {
         let img = new webp.Image()
