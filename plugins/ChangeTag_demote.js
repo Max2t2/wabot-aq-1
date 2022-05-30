30bf629bc404a1308826c237bfd25793de9728e7
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 23:47:42 -0700

    Update and rename demote.js to ChangeTag_demote.js

--- plugins/ChangeTag_demote.js
+++ plugins/ChangeTag_demote.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, participants }) => {
   let users = m.mentionedJid.filter(user => members.includes(user))
   for (let user of users) await conn.groupDemoteAdmin(m.chat, [user]).catch(console.log)
 }
-handler.help = ['demote','member','↓'].map(v => v + ' @user')
+handler.help = ['demote'].map(v => v + ' @user')
 handler.tags = ['Admins']
 
-handler.command = /^(demote|member|↓)$/i
+handler.command = /^(demote)$/i
 
 handler.group = true
 
