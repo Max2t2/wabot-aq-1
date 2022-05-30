bd8a2eeb9d7183620206dfa549a18de0c6242dbb
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 23:01:57 -0700

    Update charainfo.js

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -1,20 +1,18 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Enter query!`
+  if (!text) throw `*نام شخصیت مورد نظر وارد نشده!*`
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   let { name, alternative_names, url, image_url, type } = json.results[0]
 let charaingfo = `💬 *Name:* ${name}
 💭 *Nickname:* ${alternative_names}
-🔗 *Link*: ${url}
-👤 *Character Type*: ${type}`
+👤 *Character Type*: ${type}
+🔗 *Link*: ${url}`
 
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
 }
 handler.help = ['character <nama>']
 handler.tags = ['internet']
 handler.command = /^(chara|character)$/i
-//kyaa jangan biarkan wabot-aq terbengkalai sampai nurutomo kembali
-// Command - Re Edited -- TOXIC-DEVIL == || Character Type ||
 module.exports = handler
