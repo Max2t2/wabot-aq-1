4b4a998fc672814aa24f5947f56bc3087eda0aa1
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-29 23:23:25 -0700

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,15 +2,15 @@ let handler = function (m) {
   if (!m.quoted) throw false
   let { chat, fromMe, id, isBaileys } = m.quoted
   if (!fromMe) throw false
-  if (!isBaileys) throw '*این پیام توسط ربات ارسالد نشده!*'
+  if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
   this.deleteMessage(chat, {
     fromMe,
     id,
     remoteJid: chat
   })
 }
-handler.help = ['del(ete)']
-handler.tags = ['admin']
+handler.help = ['del', 'delete']
+handler.tags = ['info']
 
 handler.command = /^del(ete)?$/i
 
