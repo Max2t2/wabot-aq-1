1770f01ce29b2046d352a8e1bcd4e469cb883c08
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-31 06:19:15 -0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,6 +1,6 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('https://some-random-api.ml', '/tiAmqL-C'))
+  let res = await fetch(global.API('https://some-random-api.ml', '/canvas/pixelate'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.image) throw 'Err!'
