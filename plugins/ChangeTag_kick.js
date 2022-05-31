d055637a006d276988bd8c073c1b91e36921aed3
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-05-31 05:20:55 -0700

    Update and rename kick.js to ChangeTag_kick.js

--- plugins/ChangeTag_kick.js
+++ plugins/ChangeTag_kick.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
 handler.help = ['kick', '-'].map(v => v + ' @user')
-handler.tags = ['premium']
+handler.tags = ['admin']
 handler.command = /^(kick|\-)$/i
 handler.owner = false
 handler.mods = false
