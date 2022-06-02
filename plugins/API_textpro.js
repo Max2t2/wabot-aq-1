b919b9c0c6ee4a05d9007292d6426fada7c7ba78
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 07:06:54 -0700

    Update API_textpro.js

--- plugins/API_textpro.js
+++ plugins/API_textpro.js
@@ -13,6 +13,7 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
 handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
 handler.tags = ['tools']
 handler.command = /^(textpro)$/i
+handler.disabled = true
 
 module.exports = handler
 
