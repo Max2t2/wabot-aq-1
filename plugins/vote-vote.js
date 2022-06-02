8421a51c0aa67359ec395f15caf300c682f17bd8
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 05:06:12 -0700

    Update vote-vote.js

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) return conn.sendButton(m.chat, `There is no voting in this chat!`, '© surena', 'Mulai', `${usedPrefix}+vote`, conn.vote[id][3])
+    if (!(id in conn.vote)) return conn.sendButton(m.chat, `در این چت رای گیری درحال انجامی وجود ندارد!`, '© surena', 'آغاز رای گیری', `${usedPrefix}startvote این یک رای گیری بدون متن است!`, conn.vote[id][3])
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
     const wasVote = isVote.includes(m.sender)
-    if (wasVote) throw 'You have voted!'
+    if (wasVote) throw 'شما قبلا یک رای ثبت کردید!'
     if (/up/i.test(command)) {
         conn.vote[id][1].push(m.sender)
     } else if (/de/i.test(command)) {
@@ -18,13 +18,13 @@ Reason: ${reason}
 
 ╭─「 Upvote 」
 │ _Total: ${upvote.length}_
-│ ${upvote.map(u => '@' + u.split('@')[0]).join('\n')}
+│ ${upvote.map(u => '@' + u.split('@')[0]).join('\n| ')}
 ╰────
 ╭─「 Devote 」
 │ _Total: ${devote.length}_
-│ ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
+│ ${devote.map(u => '@' + u.split('@')[0]).join('\n| ')}
 ╰────
-`.trim(), '© surena', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m)
+`.trim(), '© surena', 'موافق', `${usedPrefix}upvote`, 'مخالف', `${usedPrefix}devote`, m)
 }
 handler.help = ['upvote', 'devote']
 handler.tags = ['vote']
