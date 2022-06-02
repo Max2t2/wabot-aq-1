0da186452ecdb0cdd793034c30459e23e74a46a3
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 04:25:35 -0700

    Update upload.js

--- plugins/upload.js
+++ plugins/upload.js
@@ -4,13 +4,15 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'No media found'
+  if (!mime) throw 'برروی یک فایل برای آپلود ریپلای بزنید!'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`${link}
-${media.length} Byte(s)
-${isTele ? '(No Expiry Date)' : '(Unknown)'}`)
+  m.reply(`Link: ${link}
+________________
+
+Size: ${media.length} Byte(s)
+Expire: ${isTele ? 'No Expiry Date' : 'Unknown'}`)
 }
 handler.help = ['upload (caption|reply media)']
 handler.tags = ['tools']
