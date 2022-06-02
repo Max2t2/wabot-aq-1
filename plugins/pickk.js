5013ebd72f63b6806f06273bd7f0a44008a63c07
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-01 22:29:53 -0700

    Update pickk.js

--- plugins/pickk.js
+++ plugins/pickk.js
@@ -1,5 +1,5 @@
 let handler = async (m, { text, args, participants }) => {
-    if (args[0] < 0, args.length < 2) throw 'Use this command for draw!\n\nFor example: *.pickup* 5 باهوش'
+    if (args[0] < 0, args.length < 2) throw 'از این دستور برای انجام قرعه کشی استفاده کنید!!\n\nبرای مثال:\n*.pickup* 5 باهوش'
     let users = participants.map(u => u.jid)
     m.reply(`*ما اینجا ${text.replace(args, '').trimStart()} داریم!*
     
@@ -8,7 +8,7 @@ ${new Array(Math.min(users.length, args[0])).fill().map(() => {
     return `@${users.splice(index, 1)}`
 }).join`\n`.replace(/@s.whatsapp.net/g,'')}`)
 }
-handler.help = ['pick <jumlah> <teks>']
+handler.help = ['pick <reasson> <text>']
 handler.command = /^pick/i
 handler.admin = true
 
