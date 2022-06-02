9fc35a8c481f6e1118157b20d36992d86d847ccb
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 03:11:59 -0700

    Update and rename dadu.js to dice.js

--- plugins/dice.js
+++ plugins/dice.js
@@ -9,8 +9,8 @@ const dir = [
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', '', m)
 }
-handler.help = ['dadu']
+handler.help = ['Dice']
 handler.tags = ['sticker', 'fun']
-handler.command = /^dadu$/i
+handler.command = /^dice$/i
 
 module.exports = handler
