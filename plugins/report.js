876d2281faa6a87751e89517b36fd7f57d35e8b6
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-01 23:55:38 -0700

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -4,9 +4,9 @@ let handler = async(m, { conn, text }) => {
     if (!text) throw '*لطفا گزارش خود را وارد کنید*'
     if (text.length > 300) throw 'ببخشید، پیام طولانی شد لطفا در حداکثر 300 کلمه گزارش رو ارسال کنید'
     const laporan = `*「 REPORT 」*\nSender : wa.me/${m.sender.split`@`[0]}\nReport : ${text}`
-    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '989301860610') + '989301860610@s.whatsapp.net').filter(v => v != conn.user.jid && v != '989301860610@s.whatsapp.net'))
+    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '989301860610@s.whatsapp.net').filter(v => v != conn.user.jid && v != '989301860610@s.whatsapp.net'))
     m.reply(laporan, jid)
-    m.reply(laporan, m.sender)
+    for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n\n' + readMore + '「 پیامی برای همه ✅ 」\n', true).catch(_=>_)
     m.reply('*بابت گزارش از شما ممنونیم* نتیجه گزارش به زودی از طریق واتساپ برای شما ارسال خواهد شد ✔️')
 }
 handler.help = ['bug', 'report'].map(v => v + ' <reasson>')
