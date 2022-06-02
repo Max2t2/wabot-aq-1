ad08992d9c066e8a7a68eb3b4788d3a33fe40bdd
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 00:07:59 -0700

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) throw '*لطفا گزارش خود را وارد کنید*'
     if (text.length > 300) throw 'ببخشید، پیام طولانی شد لطفا در حداکثر 300 کلمه گزارش رو ارسال کنید'
     const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
-    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '989301860610') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '@s.whatsapp.net'))
+    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender)
     m.reply('*بابت گزارش از شما ممنونیم* نتیجه گزارش به زودی از طریق واتساپ برای شما ارسال خواهد شد ✔️')
