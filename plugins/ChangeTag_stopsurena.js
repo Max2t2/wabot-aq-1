178131bed8bb86fd35021d4677ac8d263c545303
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 02:53:58 -0700

    Update and rename stopsurena.js to ChangeTag_stopsurena.js

--- plugins/ChangeTag_stopsurena.js
+++ plugins/ChangeTag_stopsurena.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn }) => {
   }
 }
 handler.help = ['stop']
-handler.tags = ['surena']
+handler.tags = ['owner']
 handler.command = /^(berhenti|stop)$/i
 handler.owner = true
 handler.mods = false
