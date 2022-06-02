117625f94b0ce0834b6984c58acf64d080458cf9
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 00:33:50 -0700

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -3,7 +3,7 @@
 let handler = async(m, { conn, text }) => {
     if (!text) throw '*لطفا گزارش خود را وارد کنید*'
     if (text.length > 300) throw 'ببخشید، پیام طولانی شد لطفا در حداکثر 300 کلمه گزارش رو ارسال کنید'
-    const laporan = `*「 REPORT 」*\nSender : wa.me/${m.sender.split`@`[0]}\nReasson : ${text}\nAsk : wa.me/${m.sender.split`@`[0]}?text=*با+سلام*+تیم+پشتیبانی+ما+از+شما+گزارشی+را+با+موضوع+_${encodeURIComponent(text)}_+دریافت+کرد.+*نتیجه+گزارش+:*+`
+    const laporan = `*「 REPORT 」*\nSender : wa.me/${m.sender.split`@`[0]}\nReasson : ${text}\n_____________________\n\nAsk :\n\n wa.me/${m.sender.split`@`[0]}?text=*با+سلام*+تیم+پشتیبانی+ما+از+شما+گزارشی+را+با+موضوع+_${encodeURIComponent(text)}_+دریافت+کرد.+*نتیجه+گزارش+:*+`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender)
