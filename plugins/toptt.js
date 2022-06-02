3f05a9a0d3dbdd10c5830fff03a3f8bd98e91575
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 03:56:20 -0700

    Update toptt.js

--- plugins/toptt.js
+++ plugins/toptt.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Reply to that audio you want to change to the voice note!`
+  if (!/video|audio/.test(mime)) throw `برروی صوتی که میخواهید به یادداشت صوتی تبدیل کنید ریپلای بزنید!`
   let media = await q.download()
   let audio = await toPTT(media, 'mp4')
   conn.sendMessage(m.chat, audio, MessageType.audio, {
