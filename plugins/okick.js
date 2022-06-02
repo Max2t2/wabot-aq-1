449dc9d75fe3c946ad94cc17f4615b12dd81f56e
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 07:26:03 -0700

    Update and rename ChangeTag_okick.js to okick.js

--- plugins/okick.js
+++ plugins/okick.js
@@ -3,16 +3,16 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick','-'].map(v => 'o' + v + ' @user')
+handler.help = ['kick'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
 handler.command = /^(okick|o\-)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
 
-handler.admin = true
+handler.admin = false
 handler.botAdmin = true
 
 handler.fail = null
