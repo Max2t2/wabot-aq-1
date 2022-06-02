acecb2c71318ab2d7afe9ba9729619aa8a79ba59
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 08:23:47 -0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -4,7 +4,7 @@ let handler = async (m) => {
     else who = m.sender
     m.reply(`${global.db.data.users[who].limit} Limit Left ಥ_ಥ`)
 }
-handler.help = ['limit [@user]']
+handler.help = ['limit @user']
 handler.tags = ['xp']
 handler.command = /^(limit)$/i
 module.exports = handler
