2ee4fdd61f88fc45b89be4cc05a60cd1ce903675
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-02 05:02:10 -0700

    Update vote-delete.js

--- plugins/vote-delete.js
+++ plugins/vote-delete.js
@@ -1,15 +1,17 @@
 let handler = async (m, { conn, usedPrefix, isAdmin, isOwner }) => {
     if (m.isGroup) {
-        if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+        //if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     }
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) return conn.sendButton(m.chat, `There is no voting in this chat!`, '© surena', 'Mulai', `${usedPrefix}+vote`, m)
+    if (!(id in conn.vote)) return conn.sendButton(m.chat, `در این چت رای گیری درحال انجامی وجود ندارد!`, '© surena', 'آغاز رای گیری', `${usedPrefix}startvote این یک رای گیری بدون متن است!`, m)
     delete conn.vote[id]
-    m.reply(`Succeed!`)
+    m.reply(`انجام شد ✅`)
 }
 handler.help = ['hapusvote']
 handler.tags = ['vote']
 handler.command = /^(delete|hapus|-)vote$/i
+handler.admin = true 
+handler.group = true
 
 module.exports = handler
